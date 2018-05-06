631dedeca3aa9f10655dec5b85a8a5b1a19174b7
sarojkumar14 <sarojkumar9048@gmail.com>
2018-05-06 12:31:31 +0530

    code modified

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 //process.env.NODE_ENV = process.env.NODE_ENV || 'qa';
 //process.env.NODE_ENV = process.env.NODE_ENV || 'uat';
 
-var config = require('./config/config');
-var express = require('./config/express');
+const config = require('./config/config');
+const express = require('./config/express');
 
-var app = express();
+const app = express();
 
 app.listen(config.serverConfig.port, () => {
     console.log(`Saroj node server app running at port ${config.serverConfig.port}`);
